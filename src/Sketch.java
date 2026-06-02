0e0678a971aec2e0c54fdd47175ef7e9601c8345
cassandral28 <cassandra.lai28@ycdsbk12.ca>
2026-06-02 12:57:51 -0400

    WIP: Click fish when blue to change background colour

--- src/Sketch.java
+++ src/Sketch.java
@@ -5,9 +5,15 @@ import processing.core.PApplet;
  * @author Cassandra Lai
  */
 public class Sketch extends PApplet {
+
+        float fishX = 313;
+        float fishY = 270;
+
+        int c1 = color(51, 102, 255);  
+        float fish = (int)random(1,4);
+
     public static void main(String[] args) {
         PApplet.main("Sketch");
-
     }
 
     @Override
@@ -70,7 +76,35 @@ public class Sketch extends PApplet {
 
     @Override
     public void draw() {
+        fill(204, 102, 0);              // Brown fill, no outline
+        noStroke();
+        rect(0, 350, 600, 200);
+        
+        fill(102, 204, 255);              // Blue fill, outline
+        stroke(100);
+        rect(100, 150, 400, 200);
 
+        if (fish == 1){
+            fill(c1);             // Blue fish
+            noStroke();
+            triangle(320, 250, 350, 230, 350, 270);
+            ellipse(300, 250, 70, 50);
+            fill(0, 0, 0);
+            ellipse(275, 250, 10, 15);
+
+            if(mousePressed && (mouseX > fishX - 38 && mouseX < fishX + 37 && mouseY > fishY - 20 && mouseY < fishY + 20)){
+                fill(204, 102, 0);              // Brown fill, no outline
+                noStroke();
+                rect(0, 350, 600, 200);
+        
+                fill(102, 204, 255);              // Blue fill, outline
+                stroke(100);
+                rect(100, 150, 400, 200);
+                background(0);
+            }
+        }
+
+        
     }
 
     /** Additional helper methods below */
