f9b3c6aff9ce8630becf1cb1cf121bc67ddf2ef8
cassandral28 <cassandra.lai28@ycdsbk12.ca>
2026-06-03 14:21:48 -0400

    WIP 2: Click fish when blue to change background colour

--- src/Sketch.java
+++ src/Sketch.java
@@ -46,6 +46,7 @@ public class Sketch extends PApplet {
             ellipse(300, 250, 70, 50);
             fill(0, 0, 0);
             ellipse(275, 250, 10, 15);
+            System.out.println("1");
         }
         else if (fish == 2){
             fill(c2);             // Yellow fish
@@ -54,6 +55,7 @@ public class Sketch extends PApplet {
             ellipse(300, 250, 70, 50);
             fill(0, 0, 0);
             ellipse(275, 250, 10, 15);
+            System.out.println("2");
         }
         else if (fish == 3){
             fill(c3);             // Purple fish
@@ -62,6 +64,7 @@ public class Sketch extends PApplet {
             ellipse(300, 250, 70, 50);
             fill(0, 0, 0);
             ellipse(275, 250, 10, 15);
+            System.out.println("3");
         }
         else if (fish == 4){
             fill(c4);             // Green fish
@@ -70,28 +73,16 @@ public class Sketch extends PApplet {
             ellipse(300, 250, 70, 50);
             fill(0, 0, 0);
             ellipse(275, 250, 10, 15);
+            System.out.println("4");
         }
         
     }
 
     @Override
     public void draw() {
-        fill(204, 102, 0);              // Brown fill, no outline
-        noStroke();
-        rect(0, 350, 600, 200);
-        
-        fill(102, 204, 255);              // Blue fill, outline
-        stroke(100);
-        rect(100, 150, 400, 200);
-
+       
         if (fish == 1){
-            fill(c1);             // Blue fish
-            noStroke();
-            triangle(320, 250, 350, 230, 350, 270);
-            ellipse(300, 250, 70, 50);
-            fill(0, 0, 0);
-            ellipse(275, 250, 10, 15);
-
+            
             if(mousePressed && (mouseX > fishX - 38 && mouseX < fishX + 37 && mouseY > fishY - 20 && mouseY < fishY + 20)){
                 fill(204, 102, 0);              // Brown fill, no outline
                 noStroke();
@@ -100,11 +91,25 @@ public class Sketch extends PApplet {
                 fill(102, 204, 255);              // Blue fill, outline
                 stroke(100);
                 rect(100, 150, 400, 200);
+
+                fill(c1);             // Blue fish
+            noStroke();
+            triangle(320, 250, 350, 230, 350, 270);
+            ellipse(300, 250, 70, 50);
+            fill(0, 0, 0);
+            ellipse(275, 250, 10, 15);
+                
                 background(0);
             }
-        }
 
+            fill(204, 102, 0);              // Brown fill, no outline
+            noStroke();
+            rect(0, 350, 600, 200);
         
+            fill(102, 204, 255);              // Blue fill, outline
+            stroke(100);
+            rect(100, 150, 400, 200);
+        }
     }
 
     /** Additional helper methods below */
